f389a62e096a2829aaae9c03b828006a3ebcee86
Alexandre Arsenault <alx.arsenault@gmail.com>
2024-05-21 01:34:03 -0400

    First commit

---
+++ js/products.js
@@ -0,0 +1,90 @@
+var products = [
+    {
+        image: "img/product-img-4.jpg",
+        fr: {
+            title: "Bois Massif",
+            info: "FR designs that make every day a feast"
+        },
+        en: {
+            title: "Name",
+            info: "Artisanal designs that make every day a feast"
+        }
+    },
+    {
+        image: "img/product-img-5.jpg",
+        fr: {
+            title: "Bois Massif",
+            info: "FR designs that make every day a feast"
+        },
+        en: {
+            title: "Name",
+            info: "Artisanal designs that make every day a feast"
+        }
+    },
+    {
+        image: "img/product-img-6.jpg",
+        fr: {
+            title: "Name",
+            info: "FR designs that make every day a feast"
+        },
+        en: {
+            title: "Name",
+            info: "Artisanal designs that make every day a feast"
+        }
+    },
+    {
+        image: "img/product-img-7.jpg",
+        fr: {
+            title: "Name",
+            info: "FR designs that make every day a feast"
+        },
+        en: {
+            title: "Name",
+            info: "Artisanal designs that make every day a feast"
+        }
+    },
+    {
+        image: "img/product-img-8.jpg",
+        fr: {
+            title: "Name",
+            info: "FR designs that make every day a feast"
+        },
+        en: {
+            title: "Name",
+            info: "Artisanal designs that make every day a feast"
+        }
+    },
+    {
+        image: "img/product-img-9.jpg",
+        fr: {
+            title: "Name",
+            info: "FR designs that make every day a feast"
+        },
+        en: {
+            title: "Name",
+            info: "Artisanal designs that make every day a feast"
+        }
+    },
+    {
+        image: "img/product-img-10.jpg",
+        fr: {
+            title: "Name",
+            info: "FR designs that make every day a feast"
+        },
+        en: {
+            title: "Name",
+            info: "Artisanal designs that make every day a feast"
+        }
+    },
+    {
+        image: "img/product-img-11.jpg",
+        fr: {
+            title: "Name",
+            info: "FR designs that make every day a feast"
+        },
+        en: {
+            title: "Name",
+            info: "Artisanal designs that make every day a feast"
+        }
+    }
+];
